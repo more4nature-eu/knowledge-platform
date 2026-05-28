536b0282a3444ef6dba3614b6ff989c529dcf439
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-05-28 19:57:47 +0200

    logic for navigating through n&s hub and returning to modify

--- m4n_knowledge_platform/needs_and_solutions_hub/models.py
+++ m4n_knowledge_platform/needs_and_solutions_hub/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.http import Http404
+from django.shortcuts import redirect
+from formtools.wizard.storage import get_storage
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import FieldPanel, InlinePanel
@@ -27,8 +29,8 @@ class Option(Orderable):
         verbose_name_plural = "Options"
 
 class Question(Orderable, ClusterableModel):
-    needs_and_solutions_hub = ParentalKey(
-        'NeedsAndSolutionsHub',
+    page = ParentalKey(
+        'NeedsAndSolutionsHubPage',
         on_delete=models.CASCADE,
         related_name='questions',
     )
@@ -42,18 +44,7 @@ class Question(Orderable, ClusterableModel):
         verbose_name = "Question"
         verbose_name_plural = "Questions"
 
-@register_setting(icon="help")
-class NeedsAndSolutionsHub(BaseGenericSetting, ClusterableModel):
-
-    panels = [
-        InlinePanel('questions', label="Question", heading="Questions", min_num=1),
-    ]
-
-    class Meta:
-        verbose_name = "Needs and solutions hub"
-
-
-class NeedsAndSolutionsHubPage(Page):
+class NeedsAndSolutionsHubPage(Page, ClusterableModel):
     intro_text = RichTextField(
         blank=True,
         help_text="Introductory text shown before the wizard starts.",
@@ -61,6 +52,7 @@ class NeedsAndSolutionsHubPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("intro_text"),
+        InlinePanel('questions', label="Question", heading="Questions", min_num=1),
     ]
 
     subpage_types = []
@@ -68,33 +60,66 @@ class NeedsAndSolutionsHubPage(Page):
     class Meta:
         verbose_name = "Needs & Solutions hub wizard page"
 
-    def _build_wizard_form_list(self):
-        questions = Question.objects.prefetch_related("options")
+    def _build_wizard_form_list(self, pks):
+        questions_by_pk = {
+            q.pk: q
+            for q in Question.objects
+                .filter(pk__in=pks)
+                .prefetch_related("options")
+        }
 
         form_list = []
         question_map = {}
 
-        for question in questions:
-            step_name = f"q_{question.pk}"
+        for pk in pks:
+            question = questions_by_pk.get(pk)
+            if question is None:
+                continue
+            step_name = f"q_{pk}"
             form_class = make_question_form(question)
             form_list.append((step_name, form_class))
             question_map[step_name] = question
 
         return form_list, question_map
 
+
     def serve(self, request, *args, **kwargs):
         if getattr(request, "is_preview", False):
             return super().serve(request, *args, **kwargs)
 
-        form_list, question_map = self._build_wizard_form_list()
+        session_key = f"wizard_questions_{self.pk}"
+
+        wizard_prefix = QuestionWizard.__name__.lower()
+
+        is_fresh_start = (
+            request.method == "GET"
+            or wizard_prefix + "-current_step" not in request.POST
+        )
+
+        if is_fresh_start:
+            pks = list(
+                Question.objects
+                    .order_by("sort_order")
+                    .values_list("pk", flat=True)
+            )
+            request.session[session_key] = pks
+            storage = get_storage(
+                "formtools.wizard.storage.session.SessionStorage",
+                wizard_prefix,
+                request,
+                None,
+            )
+            storage.reset()
+        else:
+            pks = request.session.get(session_key)
+
+        if pks is None:
+            return redirect(self.url)
+
+        form_list, question_map = self._build_wizard_form_list(pks)
 
         view = QuestionWizard.as_view(form_list=form_list)
-
-        return view(
-            request,
-            wagtail_page=self,
-            question_map=question_map,
-        )
+        return view(request, wagtail_page=self, question_map=question_map)
 
     @property
     def listing_title(self):
--- m4n_knowledge_platform/needs_and_solutions_hub/wizard.py
+++ m4n_knowledge_platform/needs_and_solutions_hub/wizard.py
@@ -1,5 +1,7 @@
-from formtools.wizard.views import SessionWizardView
+from collections import OrderedDict
+
 from django.shortcuts import render
+from formtools.wizard.views import SessionWizardView
 
 class QuestionWizard(SessionWizardView):
 
@@ -13,13 +15,23 @@ class QuestionWizard(SessionWizardView):
         context["current_question"] = self.kwargs["question_map"].get(step_name)
         context["total_questions"] = self.steps.count
 
+        previous = []
+        question_map = self.kwargs["question_map"]
+        for step_name in self.steps.all[:self.steps.index]:
+            step_data = self.get_cleaned_data_for_step(step_name)
+            if step_data:
+                question = question_map[step_name]
+                chosen_pk = step_data["answer"]
+                option = question.options.get(pk=chosen_pk)
+                previous.append({
+                    "step_name": step_name,
+                    "question": question,
+                    "option": option,
+                })
+            context["previous_answers"] = previous
         return context
 
-    def done(self, form_list, form_dict, **kwargs):
-        """
-        Correlate each step's answer back to its Question object,
-        then run your result logic.
-        """
+    def done(self, _form_list, form_dict, **_kwargs):
         question_map = self.kwargs["question_map"]
         answers = []
 
@@ -29,6 +41,7 @@ class QuestionWizard(SessionWizardView):
 
             option = question.options.get(pk=chosen_option_pk)
             answers.append({
+                "step_name": step_name,
                 "question": question,
                 "option": option,
             })
@@ -50,3 +63,38 @@ class QuestionWizard(SessionWizardView):
             "summary": f"You answered {len(answers)} question(s).",
             "answers": answers,
         }
+
+    def render_next_step(self, form, **kwargs):
+        """
+        Override this to skip to the end if all questions are answered.
+        This allows users to go back and change individual answers then
+        skip straight back to the results page rather than paging through.
+        """
+        for step in self.steps.all[self.steps.index + 1:]:
+            if not self.storage.get_step_data(step):
+                return super().render_next_step(form, **kwargs)
+            else:
+                return self.render_done(form, **kwargs)
+
+    def render_done(self, form, **kwargs):
+        """
+        This is copied and modified from the implementation in the superclass,
+        in order to ensure we do NOT clear the wizard sstate in the session on
+        completion of the wizard. This allows users to go back and modify their
+        answers to change their results.
+        """
+        final_forms = OrderedDict()
+        for form_key in self.get_form_list():
+            form_obj = self.get_form(
+                step=form_key,
+                data=self.storage.get_step_data(form_key),
+                files=self.storage.get_step_files(form_key)
+            )
+            if not form_obj.is_valid():
+                return self.render_revalidation_failure(form_key, form_obj, **kwargs)
+            final_forms[form_key] = form_obj
+
+        done_response = self.done(list(final_forms.values()), form_dict=final_forms, **kwargs)
+        # THIS IS WHERE STORAGE WOULD BE RESET IN THE ORIGINAL:
+        # self.storage.reset()
+        return done_response
